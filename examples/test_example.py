025e85a505f4d48445e2c2567fd626aad88dc6ee
Rachel Hu <goldpiggy@berkeley.edu>
2024-04-03 00:19:25 -0700

    Add Example folder and load CAMBIO API key from .env.

---
+++ examples/test_example.py
@@ -0,0 +1,30 @@
+import os
+import sys
+
+from dotenv import load_dotenv
+
+sys.path.append(".")
+sys.path.append("..")
+sys.path.append("../..")
+
+from open_parser import OpenParser  # noqa: E402
+
+if __name__ == "__main__":
+    load_dotenv()
+
+    example_apikey = os.getenv("CAMBIO_API_KEY")
+
+    example_local_file = "./test2.pdf"
+
+    op = OpenParser(example_apikey)
+
+    print("file content extraction test:")
+    content_result = op.extract(example_local_file)
+    print(type(content_result))
+    print(content_result)
+
+    print("file information QA test:")
+    example_prompt = "Return table under Investor Metrics in JSON format with year as the key and the column as subkeys."
+    qa_result = op.parse(example_local_file, example_prompt)
+    print(type(qa_result))
+    print(qa_result)
