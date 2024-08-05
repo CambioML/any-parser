a1fb99f7035b21f643f990a7b53593f656d0c322
Yifan Zhang <yifan-zhang@berkeley.edu>
2024-08-05 07:21:48 -0700

    lint

--- examples/test_example.py
+++ examples/test_example.py
@@ -1,6 +1,5 @@
 import os
 import sys
-
 from dotenv import load_dotenv
 
 sys.path.append(".")
@@ -14,13 +13,11 @@ if __name__ == "__main__":
     load_dotenv()
 
     example_apikey = os.getenv("CAMBIO_API_KEY")
-
     example_local_file = "./sample_data/test2.pdf"
 
-    op = AnyParser(example_apikey)
+    any_parser_instance = AnyParser(example_apikey)
 
     print("file/document extraction test:")
-    content_result = op.extract(example_local_file)
+    content_result = any_parser_instance.extract(example_local_file)
     print(type(content_result))
     print(content_result)
-
