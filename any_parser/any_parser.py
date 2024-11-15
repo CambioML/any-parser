0ee71257e97b20f89a858288f4d5c022865750ff
Jingyi <dellsjy@umich.edu>
2024-11-15 03:18:46 +0000

    rename parse

--- any_parser/any_parser.py
+++ any_parser/any_parser.py
@@ -20,7 +20,7 @@ class ProcessType(Enum):
     EXTRACT_TABLES = "extract_tables"
     EXTRACT_KEY_VALUE = "extract_key_value"
     EXTRACT_RESUME_KEY_VALUE = "extract_resume_key_value"
-    PARSE = "file"
+    PARSE = "parse"
     PARSE_WITH_OCR = "parse_with_ocr"
     PARSE_WITH_LAYOUT = "parse_with_layout"
 
